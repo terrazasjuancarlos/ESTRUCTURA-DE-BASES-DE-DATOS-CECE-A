75cf95cda1142da7cdd7b718cfcba4d139418a0f
terrazasjuancarlos <124410875+terrazasjuancarlos@users.noreply.github.com>
2026-03-01 00:02:59 -0700

    Update EspiralRecursiva.java

--- EspiralRecursiva.java
+++ EspiralRecursiva.java
@@ -1,39 +1,27 @@
 public class EspiralRecursiva {
-
     static int valor = 1;
-
     public static void llenarEspiral(int[][] matriz, int filaInicio, int filaFin, int colInicio, int colFin) {
 
-        // Caso base
         if (filaInicio > filaFin || colInicio > colFin) {
             return;
         }
-
-        // Fila superior
         for (int i = colInicio; i <= colFin; i++) {
             matriz[filaInicio][i] = valor++;
         }
-
-        // Columna derecha
         for (int i = filaInicio + 1; i <= filaFin; i++) {
             matriz[i][colFin] = valor++;
         }
-
-        // Fila inferior
         if (filaInicio < filaFin) {
             for (int i = colFin - 1; i >= colInicio; i--) {
                 matriz[filaFin][i] = valor++;
             }
         }
-
-        // Columna izquierda
         if (colInicio < colFin) {
             for (int i = filaFin - 1; i > filaInicio; i--) {
                 matriz[i][colInicio] = valor++;
             }
         }
 
-        // Llamada recursiva reduciendo límites
         llenarEspiral(matriz, filaInicio + 1, filaFin - 1, colInicio + 1, colFin - 1);
     }
 
